2ffe118696978d4f270b664a6941ce46c0784ff4
Stephen Bell <stephen.david.bell@googlemail.com>
2012-05-16 02:46:05 +0100

    That didn't work, trying something else

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ GlasgowMusicScene::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   devise_for :users, :skip => :registrations
   root :to => 'home#index'
+  
+  constraints(:host => /example.com/) do
+    root :to => redirect("http://www.example.com")
+    match '/*path', :to => redirect {|params| "http://www.example.com/#{params[:path]}"}
+  end
 end
